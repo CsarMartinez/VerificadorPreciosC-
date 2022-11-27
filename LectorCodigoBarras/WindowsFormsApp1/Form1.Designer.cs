70ac7b57db901368ace2ce89c3d600d320ce8a4d
CsarMartinez <118865537+CsarMartinez@users.noreply.github.com>
2022-11-27 16:14:29 -0700

    Add files via upload

---
+++ LectorCodigoBarras/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(306, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(676, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Verificador de precios";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(401, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 429);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(273, 551);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lector de Codigo de Barras";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(809, 538);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "By Cesar Martinez";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(841, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 45);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(841, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 46);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Precio";
+            this.label5.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(846, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 218);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(846, 216);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 37);
+            this.panel2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label6.Location = new System.Drawing.Point(202, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(579, 39);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Pase el código de barras por el lector";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(678, 563);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(130, 122);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(849, 440);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 80);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1086, 705);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ LectorCodigoBarras/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   
+    public partial class Form1 : Form
+    {
+        string codigo = "";
+        string ruta = "D:\\csarm\\Documents\\UTH\\ArquitecturaSoftware\\LectorCodigoBarras\\WindowsFormsApp1\\img";
+        string[,] productos =
+            {
+                { "1", "PINTURA BEREL VINIL ACRÍLICA BASE NEUTRA 19 L", "$2,195.00", "berel19l.jpg", "Pintura base neutra Berelinte Berel de 19 L, brinda un recubrimiento para superficies de yeso, cemento, mortero, entre otros; tanto en interiores como en exteriores. Hecha con vinil y acrílico que le brinda resistencia al lavado y dura hasta 7 años."},
+                { "2", "PINTURA BEHR PREMIUM PLUS MATE BASE BLANCO ULTRA PURO 18.9 L", "$3,945.00", "BEHR.jpg","Pintura 100% acrílica Behr Premium Plus, entintable en colores claros, brinda un recubrimiento especial para superficies exteriores como revestimiento de madera, vinilo y de fibra de cemento como mortero, ladrillo, mampostería y enrejado; dejando en ellas un acabado mate, ideal para disimular imperfecciones. Su fórmula ecológica, tiene cero niveles de COV y bajo olor, es resistente a la humedad y rayos UV, además de ser lavable con agua y jabón. Cuenta con secado rápido 2 horas al tacto. Su presentación es en cubeta de 18.9 L que brinda un rendimiento de 116 a 186 m2 con excelente cobertura. Para su aplicación con brocha o rodillo se debe utilizar directamente, sin diluir."},
+                { "3", "SELLADOR VINÍLICO 19 L", "$1,529.00", "sellador.jpg","Sellador vinílico 570 Berel. Diseñado especialmente para uso interior en aplanados de yeso, cemento o mortero que serán pintados por pinturas emulsionadas de cualquier tipo; su secado al tacto es de 1 hora. Su fórmula elaborada con resinas de alta calidad vinilacrílicas brinda gran adhesión, resistencia a la humedad y sellado, la mezcla diluida rinde de 3 a 5m2/L. Presentación en cubeta de 19 L."},
+                { "4", "PISTOLA ELÉCTRICA DE 900 ML NEGRO MASTER HARDWARE", "$1,129.00", "pistola.jpg","La Pistola Eléctrica Master Hardware con capacidad de 900 mililitros permite realizar trabajos de pintura con excelente calidad y acabados. Se puede emplear con todo tipo de pinturas y sobre cualquier material, trabajando tanto en interior como exterior, cuenta con ajuste de aire, volumen y abanico para adaptarse a las necesidades del usuario. Ahorra tiempo y pintura cada vez que se utiliza, gracias a su potencia de 450 W que permite pintar a una distancia de hasta 5 metros en sólo 5 minutos."},
+                { "5", "KIT SEMIPROFESIONAL DE PLÁSTICO DE 39.5 X 29 X 6.5", "$185.00", "kit.jpg","Kit semiprofesional HDX con 5 piezas, útil para realizar tareas de mantenimiento y decoración sobre los muros del hogar. Cuenta con 1 charola para cargar los rodillos y dosificar la pintura evitando desperdicios de 39.5 x 29 x 6.5 cm, 1 brocha de poliéster de 2-1/2 pulgadas (6.3 cm) y 1 maneral semiprofesional de 9-1/2 pulgadas (24.1 cm) con mango de plástico negro. Además, 2 felpas de uso general, la primera de 3/4 de pulgada (1.9 cm) para superficies rugosas y la segunda de 3/8 de pulgada (0.9 cm) para superficies lisas."}
+            };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ini()
+        {
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2, 50);
+            pictureBox1.Location = new Point(this.Width / 2 - pictureBox1.Width / 2,
+                this.Height / 2 - pictureBox1.Height / 2);
+            label2.Location = new Point(this.Width / 2 - label2.Width / 2,
+                this.Height - label2.Height);
+
+            panel1.Location = new Point(0, 0);
+            panel1.Width = this.Width;
+            panel2.Location = new Point(0, panel1.Height);
+            panel2.Width = this.Width;
+
+            label3.Location = new Point(this.Width - label3.Width,
+               this.Height - label3.Height);
+
+            label6.Location = new Point(this.Width / 2 - label6.Width / 2,
+             panel1.Height + panel2.Height + 30);
+
+            pictureBox3.Location = new Point(this.Width / 2 - pictureBox3.Width / 2,
+                this.Height - label2.Height - pictureBox3.Height);
+
+            label4.Location = new Point(600, 300);
+            label4.Width = 700;
+            label4.Height = 100;
+            label5.Location = new Point(600, 400);
+            label7.Location = new Point(600, 500);
+            label7.Width = 700;
+            label7.Height = 100;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = false;
+            label6.Visible = true;
+
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ini();
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool productFound = false;
+            if (e.KeyChar != 13)
+            {
+                codigo += e.KeyChar;
+            }
+            else
+            {
+                //MessageBox.Show("Codigo " + codigo);
+                for (int i = 0; i < 5; i++)
+                {
+                    if (codigo == productos[i,0])
+                    {
+                        //MessageBox.Show("Productos" + productos[i, 1] + productos[i, 2]);
+                        label4.Visible = true;
+                        label5.Visible = true;
+                        label6.Visible = false;
+                        label7.Visible = true;
+                        label4.Text = productos[i, 1];
+                        label5.Text = productos[i, 2];
+                        label7.Text = productos[i, 4];
+                        pictureBox1.Image = Image.FromFile(ruta + @"\" + productos[i, 3]);
+
+                        pictureBox1.Location = new Point(200,
+               this.Height / 2 - pictureBox1.Height / 2);
+
+                        productFound = true;
+                    }
+                }
+
+                codigo = "";
+
+                if (!productFound)
+                {
+                    pictureBox1.Image = Image.FromFile(ruta + @"\codigo-de-barras-creativos-png.jpg");
+                    ini();
+                    MessageBox.Show("Producto no encontrado contacte a un empleado");
+                }
+            }
+        }
+
+    }
+}
